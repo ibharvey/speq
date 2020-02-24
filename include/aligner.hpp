a41db88f4bcac68578e3a8695b09aeb1adb77223
Ian Harvey <harvei.pikes@gmail.com>
2020-02-24 13:05:57 -0600

    Small mod

--- include/aligner.hpp
+++ include/aligner.hpp
@@ -5,9 +5,8 @@
 #include <thread>
 #include <vector>
 
-#include <seqan3/alignment/pairwise/align_pairwise.hpp>
-#include <seqan3/alignment/scoring/nucleotide_scoring_scheme.hpp>
-#include <seqan3/alignment/scoring/gap_scheme.hpp>
+#include <seqan3/search/configuration/all.hpp>
+
 #include <seqan3/core/debug_stream.hpp>
 
 #include <seqan3/io/sequence_file/input.hpp>
@@ -20,5 +19,11 @@
 #include <range/v3/view/transform.hpp>
 #include <range/v3/view/for_each.hpp>
 #include <range/v3/view/repeat_n.hpp>
+#include <range/v3/view/zip.hpp>
+#include <range/v3/view/join.hpp>
 
-int speq_run(cmd_arguments args);
+#include <range/v3/algorithm/copy_backward.hpp>
+
+
+int speq_run_1(cmd_arguments args);
+int speq_run_2(cmd_arguments args);
