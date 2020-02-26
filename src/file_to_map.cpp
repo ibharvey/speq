3adb076de04f047fd810a0ba93e4d0b1264864c1
ibharvey <burstolava@yahoo.com>
2020-02-26 10:40:15 -0600

    Fixed bug in file_to_map

--- src/file_to_map.cpp
+++ src/file_to_map.cpp
@@ -6,19 +6,17 @@ std::map<size_t,std::string> file_to_map(std::filesystem::path a_path)
     std::ifstream inf(a_path);
     std::string line,variant;
     size_t genome_index;
-    std::stringstream ss;
     if(inf.is_open())
     {
         while(std::getline(inf,line))
         {
-            auto temp = ranges::views::split(line,':');
-            auto temp_it = std::begin(temp);
-            variant = *temp_it | ranges::to<std::string>;
-            temp_it++;
-            auto inds = ranges::views::split(*temp_it,',');
-            for(auto it = std::begin(inds); it != std::end(inds); it++)
+            auto col_ind = line.find(":");
+            variant = line.substr(0,col_ind);
+            auto gis = line.substr(col_ind + 1);
+            std::stringstream ss;
+            ss << gis;
+            while(ss.rdbuf()->in_avail() > 0)
             {
-                ss << *it;
                 ss >> genome_index;
                 map_out[genome_index] = variant;
             }
